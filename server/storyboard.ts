6a95d7c0530b626fd7fba3afe7492a8c07cdd0b4
Alexis Belmonte <alexis.belmonte@epitech.eu>
2023-02-08 20:48:52 +0100

    server.storyboard: log using `pino`

--- server/storyboard.ts
+++ server/storyboard.ts
@@ -1,22 +1,34 @@
-import express from 'express'
-import { Express } from 'express-serve-static-core'
+import express from 'express';
+import pino from 'pino';
+
+import { HttpCode } from './util/http';
 
 export class Storyboard {
     public static readonly PORT: number = 1987
  
-    server: Express;
+    private logger;
+    private server;
 
     constructor() {
         this.server = express();
+        this.logger = pino({
+            transport: {
+                target: 'pino-pretty',
+                options: {
+                    translateTime: "dd-mm-yyyy HH:MM:ss,l",
+                    ignore: "pid,hostname"
+                }
+            }
+        });
     }
 
     public run() {
         this.server.get('/', (request, response) => {
-            response.statusCode = 200;
+            response.statusCode = HttpCode.OK;
             response.send('Hello, world!');
         });
         this.server.listen(Storyboard.PORT, () => {
-            console.log('Now listening!');
+            this.logger.info(`Now listening on port ${Storyboard.PORT}`);
         });
     }
 }
