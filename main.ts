db5f5253b8a9a68a388c1d04c0b10f872b3dfea4
Alexis Belmonte <alexis.belmonte@epitech.eu>
2023-02-09 11:04:54 +0100

    meta: finalize `typescript-rtti` configuration

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 import { register as tsRegister } from 'typescript-paths';
 tsRegister();
 
+import 'reflect-metadata';
+
 import { Storyboard } from './server/storyboard';
 
 const server = new Storyboard()
