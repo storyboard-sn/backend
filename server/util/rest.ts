11e440934542e507b069b795946a48fe83305b5c
Alexis Belmonte <alexis.belmonte@epitech.eu>
2023-02-09 17:15:49 +0100

    server.util.rest: add `UNKNOWN` and `FAILURE` statuses

--- server/util/rest.ts
+++ server/util/rest.ts
@@ -3,7 +3,9 @@ export interface RestBody {}
 export interface RestRequest {}
 
 export enum RestStatus {
+    UNKNOWN           = 'UNKNOWN',
     SUCCESS           = 'SUCCESS',
+    FAILURE           = 'FAILURE',
     MALFORMED_REQUEST = 'MALFORMED_REQUEST',
     SERVER_ERROR      = 'SERVER_ERROR'
 }
