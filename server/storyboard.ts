d98dbe2b0939b1de74911fe841ace11dfacda6fd
Alexis Belmonte <alexis.belmonte@epitech.eu>
2023-02-09 11:06:27 +0100

    server: finalize things for now

--- server/storyboard.ts
+++ server/storyboard.ts
@@ -1,17 +1,18 @@
 import express from 'express';
-import pino from 'pino';
+import { Express } from 'express-serve-static-core';
+import pino, { Logger } from 'pino';
 
-import { HttpCode } from './util/http';
+import { UserSubrouter } from '@/route/user';
 
 export class Storyboard {
     public static readonly PORT: number = 1987
  
-    private logger;
-    private server;
+    private readonly _logger: Logger;
+    private readonly _server: Express;
 
     constructor() {
-        this.server = express();
-        this.logger = pino({
+        this._server = express();
+        this._logger = pino({
             transport: {
                 target: 'pino-pretty',
                 options: {
@@ -20,13 +21,19 @@ export class Storyboard {
                 }
             }
         });
+
+        new UserSubrouter(this);
+    }
+
+    public get logger(): Logger {
+        return this._logger;
+    }
+
+    public get server(): Express {
+        return this._server;
     }
 
     public run() {
-        this.server.get('/', (request, response) => {
-            response.statusCode = HttpCode.OK;
-            response.send('Hello, world!');
-        });
         this.server.listen(Storyboard.PORT, () => {
             this.logger.info(`Now listening on port ${Storyboard.PORT}`);
         });
