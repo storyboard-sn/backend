83be81910e0ed0599f95ed4231a2e4779c0282ec
Alexis Belmonte <alexis.belmonte@epitech.eu>
2023-02-09 13:17:27 +0100

    main: tidy code

--- main.ts
+++ main.ts
@@ -1,9 +1,6 @@
 import { register as tsRegister } from 'typescript-paths';
 tsRegister();
 
-import 'reflect-metadata';
-
 import { Storyboard } from './server/storyboard';
 
-const server = new Storyboard()
-server.run();
+Storyboard.instance.run();
