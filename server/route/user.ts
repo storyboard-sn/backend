706ca22a359efe3990107567fe6c0203b6b4799e
Alexis Belmonte <alexis.belmonte@epitech.eu>
2023-02-09 17:16:29 +0100

    server.route.user (auth): progress on the implementation

--- server/route/user.ts
+++ server/route/user.ts
@@ -1,14 +1,18 @@
 import { Router, Request, Response } from 'express';
 
-import { Storyboard } from '@/storyboard';
+import { Storyboard } from '@/server';
 
 import { Subrouter } from '@/util/arch';
 import { HttpCode } from '@/util/http';
-import { RestRequest } from '@/util/rest';
+import { RestRequest, RestResponse, RestStatus } from '@/util/rest';
 
 export interface UserAuthRequest extends RestRequest {
     tag: string;
     password: string;
+}
+
+export interface UserAuthResponse extends RestResponse {
+    
 }
 
 export class UserSubrouter extends Subrouter {
@@ -22,13 +26,18 @@ export class UserSubrouter extends Subrouter {
 
     public authenticate(request: Request, response: Response) {
         let requestObj: UserAuthRequest = request.body;
+        let responseObj: UserAuthResponse = {
+            status: RestStatus.UNKNOWN
+        };
 
-        if (!requestObj.tag || !requestObj.password)
+        if (!requestObj.tag || !requestObj.password) {
             response.status(HttpCode.BAD_REQUEST);
-        else {
+            responseObj.status = RestStatus.MALFORMED_REQUEST;
+        } else {
             response.status(HttpCode.OK);
+            responseObj.status = RestStatus.SUCCESS;
         }
 
-        response.send();
+        response.send(responseObj);
     }
 }
