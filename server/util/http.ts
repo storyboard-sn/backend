a5e1ba82df66fa52017f475fbeaa47229810310a
Alexis Belmonte <alexis.belmonte@epitech.eu>
2023-02-09 11:07:30 +0100

    server.util.http-method: add definition

--- server/util/http.ts
+++ server/util/http.ts
@@ -1,3 +1,15 @@
+export enum HttpMethod {
+    GET     = "get",
+    HEAD    = "head",
+    POST    = "post",
+    PUT     = "put",
+    DELETE  = "delete",
+    CONNECT = "connect",
+    OPTIONS = "options",
+    TRACE   = "trace",
+    PATCH   = "patch"
+}
+
 export enum HttpCode {
     CONTINUE                        = 100,
     SWITCHING_PROTOCOLS             = 101,
