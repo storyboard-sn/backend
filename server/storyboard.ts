ba698ecfdbc5c63c75270f658b83649693e20123
Alexis Belmonte <alexis.belmonte@epitech.eu>
2023-02-09 13:17:44 +0100

    server.storyboard: introduce `express` middleware

--- server/storyboard.ts
+++ server/storyboard.ts
@@ -1,8 +1,32 @@
 import express from 'express';
-import { Express } from 'express-serve-static-core';
+import { NextFunction } from 'connect';
+import { Express, Request, Response } from 'express-serve-static-core';
 import pino, { Logger } from 'pino';
 
 import { UserSubrouter } from '@/route/user';
+import { HttpCode } from '@/util/http';
+import { RestErrorResponse, RestStatus } from './util/rest';
+
+class StoryboardMiddleware {
+    public static handleError(
+        error: Error,
+        request: Request, response: Response, next: NextFunction
+    ) {
+        const body: RestErrorResponse = {
+            status:  RestStatus.SERVER_ERROR,
+            message: error.message
+        };
+
+        Storyboard.instance.logger.fatal
+            (`Unexpected error while handling '${request.originalUrl}'`);
+        
+        response
+            .status(HttpCode.INTERNAL_SERVER_ERROR)
+            .send(body);
+
+        next();
+    }
+}
 
 export class Storyboard {
     public static readonly PORT: number = 1987
@@ -21,8 +45,9 @@ export class Storyboard {
     constructor() {
         this._server = express();
 
-        this._server.use(express.json());
         this._server.use(express.urlencoded({ extended: true }));
+        this._server.use(express.json());
+        this._server.use(StoryboardMiddleware.handleError);
 
         this._logger = pino({
             transport: {
