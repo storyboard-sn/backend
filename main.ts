5390d55d1b7302ecb610a8896af5f1ef22fc8b8c
Alexis Belmonte <alexis.belmonte@epitech.eu>
2023-02-09 17:17:29 +0100

    main: handle user signals

--- main.ts
+++ main.ts
@@ -1,6 +1,10 @@
 import { register as tsRegister } from 'typescript-paths';
 tsRegister();
 
-import { Storyboard } from './server/storyboard';
+import { Storyboard } from './server/server';
 
 Storyboard.instance.run();
+
+process.on('SIGUSR2', () => Storyboard.instance.stop());
+process.on('SIGQUIT', () => Storyboard.instance.stop());
+process.on('SIGINT',  () => Storyboard.instance.stop());
