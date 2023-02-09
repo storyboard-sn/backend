313b5e0ed2507785061d0696a35b98236bd12011
Alexis Belmonte <alexis.belmonte@epitech.eu>
2023-02-09 20:30:59 +0100

    server: tidy up code

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,26 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
+import * as packageInfo from '../package.json';
+import 'dotenv/config';
 
 import express from 'express';
+import { Server, IncomingMessage, ServerResponse } from 'http';
 import { NextFunction } from 'connect';
 import { Express, Request, Response } from 'express-serve-static-core';
 
 import pino, { Logger } from 'pino';
 
-import { MongoClient } from 'mongodb';
+import { Db, MongoClient, MongoServerSelectionError } from 'mongodb';
+
+import { HttpCode } from '@/util/http';
+import { RestResponse, RestStatus } from '@/util/rest';
 
 import { UserSubrouter } from '@/route/user';
-import { HttpCode } from '@/util/http';
-import { RestErrorResponse, RestStatus } from './util/rest';
-import { Server, IncomingMessage, ServerResponse } from 'http';
 
 class StoryboardMiddleware {
     public static handleError(
         error: Error,
         request: Request, response: Response, next: NextFunction
     ) {
-        const body: RestErrorResponse = {
-            status:  RestStatus.SERVER_ERROR,
-            message: error.message
-        };
+        const body: RestResponse = { status:  RestStatus.SERVER_ERROR };
 
         Storyboard.instance.logger.fatal
             (`Unexpected error while handling '${request.originalUrl}'`);
@@ -36,21 +34,26 @@ class StoryboardMiddleware {
 }
 
 export class Storyboard {
-    public static readonly PORT: number = 1987;
-    public static readonly URL: string = `mongodb://` +
+    public static readonly PORT = 1987;
+
+    public static readonly DATABASE_URL = `mongodb://` +
         process.env.SB_DATABASE_USER + ':' +
         process.env.SB_DATABASE_PASSWORD + '@' +
-        process.env.SB_DATABASE_ADDRESS +
-        '/?appname=storyboard';
+        process.env.SB_DATABASE_ADDRESS + '/' +
+        process.env.SB_DATABASE_NAME +
+        '?appname=storyboard';
 
     private static _instance: Storyboard;
  
     private readonly _logger: Logger;
-    private readonly _server: Express;
+    private readonly _restServer: Express;
 
-    private readonly db: MongoClient;
+    private _dbServer?: MongoClient;
+    private _db?: Db;
 
-    private connection?: Server<typeof IncomingMessage, typeof ServerResponse>;
+    private _users?: UserSubrouter;
+
+    private _restListener?: Server<typeof IncomingMessage, typeof ServerResponse>;
 
     public static get instance(): Storyboard {
         if (!Storyboard._instance)
@@ -59,47 +62,70 @@ export class Storyboard {
     }
 
     constructor() {
-        this._server = express();
+        this._restServer = express();
 
-        this._server.use(express.urlencoded({ extended: true }));
-        this._server.use(express.json());
-        this._server.use(StoryboardMiddleware.handleError);
+        this._restServer.use(express.urlencoded({ extended: true }));
+        this._restServer.use(express.json());
+        this._restServer.use(StoryboardMiddleware.handleError);
 
         this._logger = pino({
             transport: {
                 target: 'pino-pretty',
                 options: {
                     translateTime: "dd-mm-yyyy HH:MM:ss,l",
-                    ignore: "pid,hostname"
+                    ignore: "pid,hostname",
                 }
-            }
+            },
+            level: 'trace'
         });
-
-        this.db = new MongoClient(Storyboard.URL);
-        this.db.db(process.env.SB_DATABASE_NAME);
-
-        this.connection = undefined;
-
-        new UserSubrouter(this);
     }
 
     public get logger(): Logger {
         return this._logger;
     }
 
-    public get server(): Express {
-        return this._server;
+    public get restServer(): Express {
+        return this._restServer;
     }
 
-    public run() {
-        this.connection = this.server.listen(Storyboard.PORT, () => {
+    public get db(): Db | undefined {
+        return this._db;
+    }
+
+    public get users(): UserSubrouter | undefined {
+        return this._users;
+    }
+
+    public async run() {
+        this.logger.info(`Hello, Storyboard! ${packageInfo.version}`);
+
+        this.logger.debug('Connecting to database');
+        this._dbServer = new MongoClient(Storyboard.DATABASE_URL);
+        this._db = this._dbServer.db();
+
+        try {
+            await this._db.command({ping: 1})
+        } catch (error) {
+            if (error instanceof Error) {
+                this.logger.fatal
+                    (`Failed to connect to database, cannot start: ${error.message}`);
+                return;
+            } else
+                throw error;
+        }
+
+        this.logger.info("Successfully created a connection to the database");
+
+        this._users = new UserSubrouter(this);
+
+        this._restListener = this.restServer.listen(Storyboard.PORT, () => {
             this.logger.info(`Now listening on port ${Storyboard.PORT}`);
         });
     }
 
     public stop() {
         this.logger.info(`Closing listener on port ${Storyboard.PORT}`);
-        this.connection?.close();
-        this.db.close();
+        this._restListener?.close();
+        this._dbServer!.close();
     }
 }
